f43a69e0ae4f286bc5a2751e4b2b2da54045bdf5
vincent-vianto <vincentvianto@gmail.com>
2020-04-07 17:10:58 +0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d';
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d')
+    .then(response => {
+        return response.json()
+    })
+
+    .then(data => {
+        console.log(data);
+        const {
+            name,
+            weather,
+            wind,
+            coord,
+            temp,
+        } = data;
+
+        const dataWeather = `
+        <div class = "col-sm-12 col-md-4">
+        <i class="fas fa-cloud-rain fa-4x"></i>
+        <br><br>
+        City Name :  ${data.name},
+        <br>
+        Weather : ${weather[0].main},
+        <br>
+        Wind : ${data.wind.speed} km/hour, 
+        <br>
+        Coordinate : ${data.coord.lon}, ${data.coord.lat}
+        <br>
+        Humidity : ${data.main.humidity}%
+        <br>
+        Pressure : ${data.main.pressure} P
+        <br>
+        Temperature :  ${data.main.temp}K
+        </div>`
+
+
+        document.getElementById('weather').innerHTML += dataWeather;
+    })
+
+// sys.name , weather ,  wind , coord , main.temp
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Anggota Kelompok
+1. Evan Charles
+2. Riyo Firsan
+3. Vincent O.V
